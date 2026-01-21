12b5eca2fef4dab675e8d736cf0f6debf20fb9fc
DinosL <konstantinos@duckdblabs.com>
2026-01-21 14:09:09 +0100

    ok...maybe not so aggressive

--- src/httpfs_curl_client.cpp
+++ src/httpfs_curl_client.cpp
@@ -422,7 +422,6 @@ private:
 		response->body = request_info->body;
 		response->url = request_info->url;
 		response->reason = HTTPUtil::GetStatusMessage(HTTPUtil::ToStatusCode(request_info->response_code));
-		response->request_error = curl_easy_strerror(res);
 		// if (!request_info->header_collection.empty()) {
 			for (auto &header : request_info->header_collection.back()) {
 				response->headers.Insert(header.first, header.second);
