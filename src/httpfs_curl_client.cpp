226d52e10f946552df3030293bfff1607aeee6df
DinosL <konstantinos@duckdblabs.com>
2026-01-21 13:21:44 +0100

    check if changing how we get the request code breaks the code

--- src/httpfs_curl_client.cpp
+++ src/httpfs_curl_client.cpp
@@ -413,7 +413,7 @@ private:
 			// TODO: request error can come from HTTPS Status code toString() value.
 			if (!request_info->header_collection.empty() &&
 			    request_info->header_collection.back().HasHeader("__RESPONSE_STATUS__")) {
-				response->request_error = request_info->header_collection.back().GetHeaderValue("__RESPONSE_STATUS__");
+				response->request_error = request_info->response_code;
 			} else {
 				response->request_error = curl_easy_strerror(res);
 			}
